f22783dde48c67116b7543e98094e793d469dfe9
Erica Goncalves <erica.goncalves@alumisoft.com.br>
2022-09-22 01:11:20 -0300

    feat: Add tema claro e provider MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      {/* Tema da site */}
+      <BrowserRouter>
+        {/* Acima -> é um componente responsável por informar a nossa aplicação que teremos um roteamento de componentes 
+        | Abaixo -> Minhas rotas de paginas da app*/}
+        <AppRoutes/>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
+
 export const AppRoutes = () => {
     return (
         <Routes>
             {/* Minhas rotas da pagina - Abaixo config cada rota - a última é serve para uma navegação perdida */}
-            <Route path="/pagina-inicial" element={<p>Pagina Inicial</p>}/>
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Pagina Inicial</Button>}/>
             <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
         </Routes>
     );
