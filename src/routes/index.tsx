00f6af6fd6a3af39d3d786540458b470ca6bdc66
Erica Goncalves <erica.goncalves@alumisoft.com.br>
2022-09-22 00:17:15 -0300

    feat: Add react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            {/* Minhas rotas da pagina - Abaixo config cada rota - a última é serve para uma navegação perdida */}
+            <Route path="/pagina-inicial" element={<p>Pagina Inicial</p>}/>
+            <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
+        </Routes>
+    );
+}
